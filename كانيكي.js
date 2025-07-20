1f704fd65a70df2cbfa38eec7898cbc8714feb9f
KhalifaKHAA <khalifaalialtahery1@gmail.com>
2025-07-20 21:28:34 +0300

    Delete كانيكي.js

--- كانيكي.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-let handler = async(m, { conn, usedPrefix, command }) => {
-let res = (await axios.get(`https://raw.githubusercontent.com/BrunoSobrino/TheMystic-Bot-MD/master/src/JSON/anime-keneki.json`)).data  
-let url = await res[Math.floor(res.length * Math.random())]
-conn.sendButton(m.chat, "𝑭𝑳𝑶𝑩𝑨⇢😼⇠ 𝑩𝑶𝑻 ❞┊", author, url, [['⚽ التالي', `${usedPrefix + command}`]], m)}
-handler.help = ['anna']
-handler.tags = ['internet']
-handler.command = /^(keneki|كانيكي)$/i
-export default handler
